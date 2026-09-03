8ee0da6135fd4082dc5f6179fcd385badb7cd614
fukashigiNo <zemvikak@gmail.com>
2026-09-03 11:03:39 +0500

    S-M: System Modified tsup config for entry for VUE and REACT

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/react/index.ts'], // Точка входа в нашу библиотеку
-  format: ['cjs', 'esm'],  // Собираем для старых (CommonJS) и новых (ES Modules) проектов
-  dts: true,               // Генерируем типы TypeScript
+  entry: {
+    react: 'src/react/index.ts',
+    vue: 'src/vue/index.ts'
+  }, 
+  format: ['cjs', 'esm'],
+  dts: true,
   splitting: false,
   sourcemap: true,
-  clean: true,             // Очищать папку dist перед каждой сборкой
-  external: ['react'],     // React не должен попадать в бандл, он должен быть установлен у пользователя
+  clean: true,
+  external: ['react', 'vue'],
 });
