292ceffc9055b4ff5b961c9f6c779751adac8c81
fukashigiNo <zemvikak@gmail.com>
2026-09-04 22:33:33 +0500

    M: Modidifed added new fileds for exports and entries

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: {
     react: 'src/react/index.ts',
-    vue: 'src/vue/index.ts'
+    vue: 'src/vue/index.ts',
+    angular: 'src/angular/index.ts' 
   }, 
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  external: ['react', 'vue'],
+  external: ['react', 'vue', '@angular/core'], 
 });
