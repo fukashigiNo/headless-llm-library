bbc1ea0aa10da6a15b9705864b2292b064e3fd59
fukashigiNo <zemvikak@gmail.com>
2026-09-05 22:27:28 +0500

    S-M: Modifies vitest for work with angular

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
+import swc from 'unplugin-swc';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    swc.vite({
+      jsc: {
+        parser: {
+          syntax: "typescript",
+          decorators: true,
+        },
+        transform: {
+          decoratorMetadata: true,
+        },
+      },
+    }),
+  ],
   test: {
-    environment: 'happy-dom', 
-    globals: true, 
-  },
+    environment: 'happy-dom',
+    globals: true,
+  }
 });
