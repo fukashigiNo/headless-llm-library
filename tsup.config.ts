3485c313960b40122ec14793055fbd0a2e1a3fa9
fukashigiNo <zemvikak@gmail.com>
2026-09-22 19:27:02 +0500

    S-F: FIxed major bug with vue

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  tsconfig: 'tsconfig.build.json',
   entry: {
     react: 'src/react/index.ts',
     vue: 'src/vue/index.ts',
-    angular: 'src/angular/index.ts' 
-  }, 
+    angular: 'src/angular/index.ts',
+  },
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  external: ['react', 'vue', '@angular/core'], 
+  external: ['react', 'react-dom', 'vue', '@angular/core'],
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.js',
+    };
+  },
 });
