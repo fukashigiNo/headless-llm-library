ec5d8febd9587078faca7ea1ea007fe72188f8ab
fukashigiNo <zemvikak@gmail.com>
2026-09-22 19:27:29 +0500

    S-F: Fixed bugs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import swc from 'unplugin-swc';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
     vue(),
+    dts({
+      tsconfigPath: './tsconfig.build.json', 
+
+    }),
     swc.vite({
       jsc: {
         parser: {
